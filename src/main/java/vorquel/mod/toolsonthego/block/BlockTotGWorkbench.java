87aa330b6f9e609f8ab34b1b37c5bfdb0afb7516
Vorquel <vorquel@gmail.com>
2016-01-30 16:42:02 -0700

    Finish crafting tables

--- src/main/java/vorquel/mod/toolsonthego/block/BlockTotGWorkbench.java
+++ src/main/java/vorquel/mod/toolsonthego/block/BlockTotGWorkbench.java
@@ -1,11 +1,15 @@
 package vorquel.mod.toolsonthego.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockWorkbench;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ContainerWorkbench;
+import net.minecraft.stats.StatList;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
@@ -19,6 +23,38 @@ public class BlockTotGWorkbench extends Block {
     
     @Override
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ) {
-        return Blocks.crafting_table.onBlockActivated(worldIn, pos, state, playerIn, side, hitX, hitY, hitZ);
+        if (worldIn.isRemote)
+        {
+            return true;
+        }
+        else
+        {
+            playerIn.displayGui(new InterfaceCraftingTable(worldIn, pos));
+            playerIn.triggerAchievement(StatList.field_181742_Z);
+            return true;
+        }
+    }
+    
+    private class InterfaceCraftingTable extends BlockWorkbench.InterfaceCraftingTable {
+        
+        protected World world;
+        protected BlockPos position;
+        
+        public InterfaceCraftingTable(World worldIn, BlockPos pos) {
+            super(worldIn, pos);
+            world = worldIn;
+            position = pos;
+        }
+        
+        @Override
+        public Container createContainer(InventoryPlayer playerInventory, EntityPlayer playerIn) {
+            return new ContainerWorkbench(playerInventory, world, position) {
+                
+                @Override
+                public boolean canInteractWith(EntityPlayer playerIn) {
+                    return playerIn.getDistanceSq(position.getX() + 0.5, position.getY() + 0.5, position.getZ() + 0.5) <= 64;
+                }
+            };
+        }
     }
 }
--- src/main/java/vorquel/mod/toolsonthego/helper/TotGBlocks.java
+++ src/main/java/vorquel/mod/toolsonthego/helper/TotGBlocks.java
@@ -2,6 +2,7 @@ package vorquel.mod.toolsonthego.helper;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 import vorquel.mod.toolsonthego.block.BlockTotGTorch;
 import vorquel.mod.toolsonthego.block.BlockTotGWorkbench;
 
@@ -13,8 +14,8 @@ public class TotGBlocks {
     
     public static final Block torchStone = new BlockTotGTorch().setUnlocalizedName(MOD_ID + ".torchStone").setLightLevel(0.9375F);
     public static final Block torchNetherrack = new BlockTotGTorch().setUnlocalizedName(MOD_ID + ".torchNetherrack").setLightLevel(0.9375F);
-    public static final Block plankStone = new Block(Material.rock).setUnlocalizedName(MOD_ID + ".plankStone").setHardness(2).setStepSound(soundTypePiston);
-    public static final Block plankNetherrack = new Block(Material.rock).setUnlocalizedName(MOD_ID + ".plankNetherrack").setHardness(2).setStepSound(soundTypePiston);
+    public static final Block plankStone = new Block(Material.rock).setUnlocalizedName(MOD_ID + ".plankStone").setCreativeTab(CreativeTabs.tabBlock).setHardness(2).setStepSound(soundTypePiston);
+    public static final Block plankNetherrack = new Block(Material.rock).setUnlocalizedName(MOD_ID + ".plankNetherrack").setCreativeTab(CreativeTabs.tabBlock).setHardness(2).setStepSound(soundTypePiston);
     public static final Block craftingTableStone = new BlockTotGWorkbench().setUnlocalizedName(MOD_ID + ".workbenchStone").setHardness(2.5f).setStepSound(soundTypePiston);
     public static final Block craftingTableNetherrack = new BlockTotGWorkbench().setUnlocalizedName(MOD_ID + ".workbenchNetherrack").setHardness(2.5f).setStepSound(soundTypePiston);
     
@@ -23,7 +24,7 @@ public class TotGBlocks {
         proxy.registerBlock(torchNetherrack, "torch_netherrack");
         proxy.registerBlock(plankStone, "plank_stone");
         proxy.registerBlock(plankNetherrack, "plank_netherrack");
-        proxy.registerBlock(craftingTableStone, "workbench_stone");
-        proxy.registerBlock(craftingTableNetherrack, "workbench_netherrack");
+        proxy.registerBlock(craftingTableStone, "crafting_table_stone");
+        proxy.registerBlock(craftingTableNetherrack, "crafting_table_netherrack");
     }
 }
