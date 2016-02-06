c0a2eb8378815f837268a29481568a6614758237
Vorquel <vorquel@gmail.com>
2016-02-06 13:20:24 -0700

    Finish new items. Bump version

--- src/main/java/vorquel/mod/toolsonthego/helper/TotGRecipes.java
+++ src/main/java/vorquel/mod/toolsonthego/helper/TotGRecipes.java
@@ -2,37 +2,61 @@ package vorquel.mod.toolsonthego.helper;
 
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 import static net.minecraft.init.Blocks.*;
-import static net.minecraft.init.Items.coal;
-import static net.minecraft.init.Items.glowstone_dust;
+import static net.minecraft.init.Items.*;
 import static vorquel.mod.toolsonthego.helper.TotGBlocks.*;
-import static vorquel.mod.toolsonthego.helper.TotGItems.stickNRack;
-import static vorquel.mod.toolsonthego.helper.TotGItems.stickStone;
+import static vorquel.mod.toolsonthego.helper.TotGItems.*;
 
 public class TotGRecipes {
     
     public static void init() {
-        // stick recipes
+        // sticks
         GameRegistry.addRecipe(new ItemStack(stickStone, 2), "x", "x", 'x', cobblestone);
         GameRegistry.addRecipe(new ItemStack(stickStone, 4), "x", "x", 'x', stone);
         GameRegistry.addRecipe(new ItemStack(stickStone, 4), "x", "x", 'x', plankStone);
         GameRegistry.addRecipe(new ItemStack(stickNRack, 2), "x", "x", 'x', netherrack);
         GameRegistry.addRecipe(new ItemStack(stickNRack, 4), "x", "x", 'x', plankNRack);
-        // torch recipes
+        // torches
         GameRegistry.addRecipe(new ItemStack(torchStone, 4), "x", "/", 'x', coal, '/', stickStone);
         GameRegistry.addRecipe(new ItemStack(torchNRack, 4), "x", "/", 'x', glowstone_dust, '/', stickNRack);
-        // plank recipes
+        // planks
         GameRegistry.addRecipe(new ItemStack(plankStone, 2), "//", "//", '/', stickStone);
         GameRegistry.addRecipe(new ItemStack(plankNRack, 2), "//", "//", '/', stickNRack);
-        // crafting table recipes
+        // crafting tables
         GameRegistry.addRecipe(new ItemStack(craftingTableStone), "xx", "xx", 'x', plankStone);
         GameRegistry.addRecipe(new ItemStack(craftingTableNRack), "xx", "xx", 'x', plankNRack);
-        // ladder recipes
+        // ladders
         GameRegistry.addRecipe(new ItemStack(ladderStone, 3), "/ /", "///", "/ /", '/', stickStone);
         GameRegistry.addRecipe(new ItemStack(ladderNRack, 3), "/ /", "///", "/ /", '/', stickNRack);
-        // stair recipes
+        // stairs
         GameRegistry.addRecipe(new ItemStack(stairPlankStone, 4), "x  ", "xx ", "xxx", 'x', plankStone);
         GameRegistry.addRecipe(new ItemStack(stairPlankNRack, 4), "x  ", "xx ", "xxx", 'x', plankNRack);
+        // bowls
+        GameRegistry.addRecipe(new ItemStack(bowlStone, 4), "x x", " x ", 'x', plankStone);
+        GameRegistry.addRecipe(new ItemStack(bowlNRack, 4), "x x", " x ", 'x', plankNRack);
+        // stews
+        GameRegistry.addShapelessRecipe(new ItemStack(stewMushroomStone), bowlStone, brown_mushroom, red_mushroom);
+        GameRegistry.addShapelessRecipe(new ItemStack(stewMushroomNRack), bowlNRack, brown_mushroom, red_mushroom);
+        GameRegistry.addRecipe(new ItemStack(stewRabbitStone), " r ", "cpm", " b ", 'b', bowlStone, 'c', carrot, 'm', brown_mushroom, 'p', baked_potato, 'r', cooked_rabbit);
+        GameRegistry.addRecipe(new ItemStack(stewRabbitStone), " r ", "cpm", " b ", 'b', bowlStone, 'c', carrot, 'm', red_mushroom, 'p', baked_potato, 'r', cooked_rabbit);
+        GameRegistry.addRecipe(new ItemStack(stewRabbitNRack), " r ", "cpm", " b ", 'b', bowlNRack, 'c', carrot, 'm', brown_mushroom, 'p', baked_potato, 'r', cooked_rabbit);
+        GameRegistry.addRecipe(new ItemStack(stewRabbitNRack), " r ", "cpm", " b ", 'b', bowlNRack, 'c', carrot, 'm', red_mushroom, 'p', baked_potato, 'r', cooked_rabbit);
+        // swords
+        GameRegistry.addRecipe(new ItemStack(swordStone), "x", "x", "/", 'x', cobblestone, '/', stickStone);
+        GameRegistry.addRecipe(new ItemStack(swordNRack), "x", "x", "/", 'x', netherrack, '/', stickNRack);
+        // picks
+        GameRegistry.addRecipe(new ItemStack(pickStone), "xxx", " / ", " / ", 'x', cobblestone, '/', stickStone);
+        GameRegistry.addRecipe(new ItemStack(pickNRack), "xxx", " / ", " / ", 'x', netherrack, '/', stickNRack);
+        // axes
+        GameRegistry.addRecipe(new ItemStack(axStone), "xx", "x/", " /", 'x', cobblestone, '/', stickStone);
+        GameRegistry.addRecipe(new ItemStack(axNRack), "xx", "x/", " /", 'x', netherrack, '/', stickNRack);
+        // shovels
+        GameRegistry.addRecipe(new ItemStack(shovelStone), "x", "/", "/", 'x', cobblestone, '/', stickStone);
+        GameRegistry.addRecipe(new ItemStack(shovelNRack), "x", "/", "/", 'x', netherrack, '/', stickNRack);
+        // hoes
+        GameRegistry.addRecipe(new ItemStack(hoeStone), "xx", " /", " /", 'x', cobblestone, '/', stickStone);
+        GameRegistry.addRecipe(new ItemStack(hoeNRack), "xx", " /", " /", 'x', netherrack, '/', stickNRack);
     }
 }
