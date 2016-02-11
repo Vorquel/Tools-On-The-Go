4d386fba5d42c5a202b9b153f62da0eabbe5e45d
Vorquel <vorquel@gmail.com>
2016-02-11 08:45:55 -0700

    Fix ladders being washed away

--- src/main/java/vorquel/mod/toolsonthego/block/BlockTotGLadder.java
+++ src/main/java/vorquel/mod/toolsonthego/block/BlockTotGLadder.java
@@ -1,5 +1,12 @@
 package vorquel.mod.toolsonthego.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockLadder;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
-public class BlockTotGLadder extends BlockLadder {}
+public class BlockTotGLadder extends BlockLadder {
+    public BlockTotGLadder() {
+        ReflectionHelper.setPrivateValue(Block.class, this, Material.rock, "blockMaterial", "field_149764_J");
+    }
+}
